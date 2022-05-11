77dee514fc618819b97e8036f6faec9b8be55c5f
Alessandro Senese <alessandro.senese@formidable.com>
2022-05-11 14:44:30 +0100

    add internal stuff

---
+++ lib/internal/debug.ts
@@ -0,0 +1,7 @@
+export const amaNoUndefined = <T>(properties: T, property: keyof T) => {
+  if (__DEV__ && properties[property] === undefined) {
+    console.error(`AMA: Please specify the "${property}" property`);
+
+    throw new Error(`AMA: The property "${property}" cannot be UNDEFINED`);
+  }
+};
