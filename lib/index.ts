55f60cb210f3121c36356a4f241090f01a400709
Alessandro Senese <alessandro.senese@formidable.com>
2022-05-11 14:49:34 +0100

    fix linter

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,4 @@
 export { AMAProvider, useAMAContext } from './components/AMAProvider';
-export * from './components/Button';
 export * from './components/Pressable';
 export * from './components/Text';
 export * from './hooks/useA11yFocus';
