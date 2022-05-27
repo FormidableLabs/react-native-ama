a4eef3f706c21e8f536c5d67dce38d1c3db69453
Alessandro Senese <alessandro.senese@formidable.com>
2022-05-27 15:01:37 +0100

    fix: overlay focus

--- lib/components/Pressable.tsx
+++ lib/components/Pressable.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import {
   AccessibilityRole,
   AccessibilityState,
-  Omit,
   Pressable as RNPressable,
   PressableProps as RNPressableProps,
 } from 'react-native';
@@ -22,39 +21,44 @@ export type PressableProps = Omit<
     accessibilityLabel: string;
   };
 
-export const Pressable: React.FC<PressableProps> = ({ children, ...rest }) => {
-  const accessibilityState: AccessibilityState = {
-    disabled: rest.disabled,
-    selected: rest.selected,
-    checked: rest.checked,
-    busy: rest.busy,
-    expanded: rest.expanded,
-  };
+export const Pressable = React.forwardRef<typeof RNPressable, PressableProps>(
+  ({ children, ...rest }, ref) => {
+    const accessibilityState: AccessibilityState = {
+      disabled: rest.disabled,
+      selected: rest.selected,
+      checked: rest.checked,
+      busy: rest.busy,
+      expanded: rest.expanded,
+    };
 
-  __DEV__ && noUndefined(rest, 'accessibilityRole', 'NO_ACCESSIBILITY_ROLE');
-  __DEV__ && noUndefined(rest, 'accessibilityLabel', 'NO_ACCESSIBILITY_LABEL');
+    __DEV__ && noUndefined(rest, 'accessibilityRole', 'NO_ACCESSIBILITY_ROLE');
+    __DEV__ &&
+      noUndefined(rest, 'accessibilityLabel', 'NO_ACCESSIBILITY_LABEL');
 
-  __DEV__ &&
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    React.useEffect(() => {
-      const style = rest?.style;
+    __DEV__ &&
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      React.useEffect(() => {
+        const style = rest?.style;
 
-      if (typeof style === 'function') {
-        contrastChecker(style({ pressed: false }), children);
-        contrastChecker(style({ pressed: true }), children);
-      } else {
-        contrastChecker(rest.style, children);
-      }
+        if (typeof style === 'function') {
+          contrastChecker(style({ pressed: false }), children);
+          contrastChecker(style({ pressed: true }), children);
+        } else {
+          contrastChecker(rest.style, children);
+        }
 
-      accessibilityLabelChecker(rest.accessibilityLabel);
-    }, [rest.style, rest.accessibilityLabel, children]);
+        accessibilityLabelChecker(rest.accessibilityLabel);
+      }, [rest.style, rest.accessibilityLabel, children]);
 
-  return (
-    <RNPressable
-      accessibilityState={accessibilityState}
-      onLayout={checkMinimumSize}
-      {...rest}>
-      {children}
-    </RNPressable>
-  );
-};
+    return (
+      <RNPressable
+        accessibilityState={accessibilityState}
+        onLayout={checkMinimumSize}
+        // @ts-ignore
+        ref={ref}
+        {...rest}>
+        {children}
+      </RNPressable>
+    );
+  },
+);
