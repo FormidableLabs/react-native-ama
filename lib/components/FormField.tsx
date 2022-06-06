6ad45208a4f7a960d9197fa0cfd0e246e039a943
Alessandro Senese <alessandro.senese@formidable.com>
2022-06-06 17:11:02 +0100

    fix: form field focus (#28)

--- lib/components/FormField.tsx
+++ lib/components/FormField.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { View, ViewProps } from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  TouchableWithoutFeedbackProps,
+} from 'react-native';
 
 import { useFormField } from '../hooks/useFormField';
 
-type FormFieldProps = ViewProps & {
+type FormFieldProps = TouchableWithoutFeedbackProps & {
   children: React.ReactNode;
 };
 
-const FormFieldBase = React.forwardRef<View, FormFieldProps>(
-  ({ children, ...props }, forwardedRef) => {
-    const viewRef = React.useRef<React.ElementRef<typeof View> | null>(null);
+const FormFieldBase: React.FC<FormFieldProps> = ({ children, ...props }) => {
+  const viewRef = React.useRef<React.ElementRef<
+    typeof TouchableWithoutFeedback
+  > | null>(null);
 
-    React.useImperativeHandle(forwardedRef, () => viewRef.current!);
+  useFormField({ ref: viewRef, hasFocusCallback: false });
 
-    useFormField({ ref: viewRef, hasFocusCallback: false });
-
-    return (
-      <View {...props} focusable={true} accessible={true} ref={viewRef}>
-        {children}
-      </View>
-    );
-  },
-);
+  return (
+    <TouchableWithoutFeedback {...props} ref={viewRef}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
+};
 
 export const FormField = React.memo(FormFieldBase);
