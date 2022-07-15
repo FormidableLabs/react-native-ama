7ff8b74ab57d5b1950dbbae8fcd060e47e370151
Kadi Kraman <hellokadi@gmail.com>
2022-07-15 16:17:55 +0100

    chore: add main queue setup func (#49)

--- ios/Ama.swift
+++ ios/Ama.swift
@@ -1,6 +1,11 @@
 @objc(Ama)
 class Ama: NSObject {
 
+    @objc
+    static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
