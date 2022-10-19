83745a4ca0e204a28a4416b6abe7e2435aa973ed
ceceppa <senesealessandro@gmail.com>
2022-10-19 14:24:18 +0100

    chore: shared src folder

--- examples/legacy/babel.config.js
+++ examples/legacy/babel.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const pak = require('../package.json');
+const pak = require('../../package.json');
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
@@ -9,7 +9,8 @@ module.exports = {
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+          [pak.name]: path.join(__dirname, '../..', pak.source),
+          src: path.join(__dirname, '..'),
         },
       },
     ],
--- examples/legacy/metro.config.js
+++ examples/legacy/metro.config.js
@@ -1,17 +1,32 @@
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
-const pak = require('../package.json');
+const pak = require('../../package.json');
+const myPackage = require('./package.json');
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '../..');
+const parent = path.resolve(__dirname, '..');
 
 const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
+/**
+ * Need to tell metro where to find all the dependencies, otherwise is going to look into
+ * the "../../node_modules" starting from the parent folder
+ */
+const allModules = [
+    ...Object.keys({
+        ...pak.peerDependencies,
+    }),
+    ...Object.keys({
+        ...myPackage.devDependencies
+    })
+];
+
 module.exports = {
   projectRoot: __dirname,
-  watchFolders: [root],
+  watchFolders: [root, parent],
 
   // We need to make sure that only one version is loaded for peerDependencies
   // So we blacklist them at the root, and alias them to the versions in example's node_modules
@@ -23,8 +38,10 @@ module.exports = {
       )
     ),
 
-    extraNodeModules: modules.reduce((acc, name) => {
+  extraNodeModules: allModules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
+
+      console.info(acc);
       return acc;
     }, {}),
   },
