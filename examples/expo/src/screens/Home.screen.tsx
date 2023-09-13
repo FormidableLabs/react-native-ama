3349df411b2f97f9a7eda0e161dbcf120e2ee9ed
JDMathew <justindavidmathew@gmail.com>
2023-09-12 23:49:09 -0700

    Adding test package to expo

--- examples/expo/src/screens/Home.screen.tsx
+++ examples/expo/src/screens/Home.screen.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { greeting } from 'test-package-greeting';
 
 export const HomeScreen = () => {
-  return <View style={styles.view}></View>;
+  return (
+    <View style={styles.view}>
+      <Text>{greeting}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   view: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     // paddingVertical: theme.padding.normal, // Add shared theme
   },
 });
