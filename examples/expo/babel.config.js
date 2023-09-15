be6ca244ed40bc3a168905103d2b87af1c055979
Justin D Mathew <justindavidmathew@gmail.com>
2023-09-14 18:47:27 -0700

    Adding react-native-reanimated (#159)

--- examples/expo/babel.config.js
+++ examples/expo/babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo',
+    'react-native-reanimated/plugin', // reanimated plugin must be listed last
+  ],
   };
 };
