3dfd5906f2f9ad4648e5d88306c0e63203e1ddd5
JDMathew <justindavidmathew@gmail.com>
2023-09-20 10:24:24 -0700

    Using shared ui

--- examples/bare/src/App.tsx
+++ examples/bare/src/App.tsx
@@ -1,15 +1,16 @@
 import 'react-native-gesture-handler';
-
-import React from 'react';
-
 import { AppNavigator } from './AppNavigator';
 import { AMAProvider } from '@react-native-ama/core';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <AMAProvider>
-      <AppNavigator />
-    </AMAProvider>
+    <GestureHandlerRootView style={{ flexGrow: 1 }}>
+      <AMAProvider>
+        <AppNavigator />
+      </AMAProvider>
+    </GestureHandlerRootView>
   );
 };
 
