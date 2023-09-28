07d4c00bd5fc12c5c66e11d51edebaecc556144d
JDMathew <justindavidmathew@gmail.com>
2023-09-28 16:48:07 -0700

    updating stack navigation package to latest

--- examples/bare/src/utils/useTypedNavigation.tsx
+++ examples/bare/src/utils/useTypedNavigation.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
-import type { StackNavigationProp } from '@react-navigation/stack';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import type { RootStackParamList } from '../types';
 
 export const useTypedNavigation = () =>
-  useNavigation<StackNavigationProp<RootStackParamList>>();
+  useNavigation<NativeStackNavigationProp<RootStackParamList>>();
