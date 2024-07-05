47e395e8f0c548b918703dd21b3833089619ab68
JDMathew <justindavidmathew@gmail.com>
2024-07-04 22:53:41 -0700

    Add react import

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 
 import { AMAProvider } from '@react-native-ama/core';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 import { AppNavigator } from './src/AppNavigation';
 
