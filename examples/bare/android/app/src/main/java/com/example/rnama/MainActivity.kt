3a9031e40c365c0ac3dea6d8588598bb7d1333e6
JDMathew <justindavidmathew@gmail.com>
2024-08-23 17:26:07 -0700

    fix type in string name

--- examples/bare/android/app/src/main/java/com/example/rnama/MainActivity.kt
+++ examples/bare/android/app/src/main/java/com/example/rnama/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "RnDiffApp"
+  override fun getMainComponentName(): String = "AMA Example"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
