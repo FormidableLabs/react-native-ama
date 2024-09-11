0f66f680527b2782ba374ab0dd4380481d0d4b5a
JDMathew <justindavidmathew@gmail.com>
2024-09-11 15:51:26 -0700

    add ignore to babel files

--- babel.base.js
+++ babel.base.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
--- examples/expo/babel.config.js
+++ examples/expo/babel.config.js
@@ -1,9 +1,10 @@
-module.exports = function(api) {
+// eslint-disable-next-line
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-    'react-native-reanimated/plugin', // reanimated plugin must be listed last
+      'react-native-reanimated/plugin', // reanimated plugin must be listed last
     ],
   };
 };
