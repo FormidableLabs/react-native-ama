5be48fa67cc6533f3afbf52ae6ec9222833a2e53
JDMathew <justindavidmathew@gmail.com>
2024-09-19 15:03:02 -0700

    export Carousel and useCarousel

--- packages/extras/src/index.ts
+++ packages/extras/src/index.ts
@@ -1,6 +1,8 @@
 // Components
 export { BottomSheet } from './components/BottomSheet';
+export { Carousel } from './components/Carousel';
 
 // Hooks
 export { useBottomSheetGestureHandler } from './hooks/useBottomSheetGestureHandler';
 export { useKeyboard } from './hooks/useKeyboard';
+export { useCarousel } from './hooks/useCarousel';
