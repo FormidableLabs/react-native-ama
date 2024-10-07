48888717521d96bab437feff8063dcf0c7880004
JDMathew <justindavidmathew@gmail.com>
2024-10-07 00:30:37 -0700

    fix GestureHandlerRootView issue on Android

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -1,16 +1,17 @@
-import 'react-native-gesture-handler';
-
 import { AMAProvider } from '@react-native-ama/core';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { AppNavigator } from './src/AppNavigation';
 
 export default function App() {
   return (
-    <AMAProvider>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </AMAProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AMAProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </AMAProvider>
+    </GestureHandlerRootView>
   );
 }
