593bb98fafea614314edf328c0d036d360997c0d
JDMathew <justindavidmathew@gmail.com>
2024-10-23 18:40:47 +0100

    Add initial LoadingScreen

---
+++ examples/shared/src/screens/Loading.screen.tsx
@@ -0,0 +1,32 @@
+import { Loading } from '@react-native-ama/extras';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { CTAPressable } from '../components/CTAPressable';
+import { theme } from '../theme';
+
+export const LoadingScreen = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const triggerLoading = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Loading isLoading={isLoading} />
+      <CTAPressable title="Trigger Loading" onPress={triggerLoading} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: theme.padding.big,
+    paddingTop: theme.padding.big,
+  },
+});
