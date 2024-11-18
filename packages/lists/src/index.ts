71cb3128eb5bc6efc41804e5dbe23441fb3cd84e
Justin D Mathew <justindavidmathew@gmail.com>
2024-11-17 23:39:00 -0800

    Updating Lists to us RN over native module ListWrapper (#269)
    * remove ListWrapper from components
    
    * Remove ListWrapper comp
    
    * Remove ListWrapper doc
    
    * remove export of ListWrapper
    
    * remove ref to ListWrapper
    
    * Added changesets

--- packages/lists/src/index.ts
+++ packages/lists/src/index.ts
@@ -1,7 +1,6 @@
 // Components
 export { DynamicFlatList } from './components/DynamicFlatList';
 export { FlatList } from './components/FlatList';
-export { ListWrapper } from './components/ListWrapper';
 export { StaticFlatList } from './components/StaticFlatList';
 
 // Hooks
