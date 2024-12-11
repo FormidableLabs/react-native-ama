1dc4aa5a3a353287218490e808882e2ad93f44e9
Eli Zibin <elizibin@gmail.com>
2024-12-11 08:10:38 -0800

    Update postinstall.js

--- packages/core/src/postinstall.js
+++ packages/core/src/postinstall.js
@@ -9,10 +9,10 @@ const isMonorepo = initCwd.includes(path.join(packageRoot, '..', '..', '..'));
 
 // If running in the monorepo context, skip the postinstall script
 if (isMonorepo) {
-  console.log('Skipping postinstall: Running within monorepo');
+  console.log('[AMA] Skipping postinstall: Running within monorepo');
   process.exit(0);
 }
 
 // If installed as a standalone dependency, run the postinstall logic
-console.log('Running postinstall: User installation');
+console.log('[AMA] Running postinstall: User installation');
 require('./../dist/scripts/create-config.js');
